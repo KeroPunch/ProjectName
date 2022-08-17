54a98eaa650f5474f6b500a9922bd864f9edcaf1
KeroPunch <de.kritsakorn_st@tni.ac.th>
2022-08-17 10:47:04 +0700

    commit 17-8-65

---
+++ screens/FirstPage.js
@@ -0,0 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// import HomeScreen from "./screens/HomeScreen";
+// import AboutScreen from "./screens/AboutScreen";
+import React, { useState } from "react";
+
+import FirstPage from "./screens/FirstPage";
+import SecondPage from "./screens/SecondPage";
+
+
+
+// <>  </> this is
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#008b7f" },
+          headerTintColor: "#ffff",
+          headerTitleStyle: { fontWeight: "bold", fontSize: 40 },
+        }}
+      >
+        <Stack.Screen name="First Page" component={FirstPage} />
+        <Stack.Screen name="Second Page" component={SecondPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: "center",
+    fontSize: 16,
+    marginVertical: 10,
+  },
+});
